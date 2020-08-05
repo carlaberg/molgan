477fcbadeddfa186f10daf23b132451b831ab426
carlaberg <mail.carl.aberg@gmail.com>
2020-08-05 20:03:54 +0200

    Switched to classes and added method modifiers

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
 import message from './message'
 console.log(message)
+
+// import scripts
+import { AudioProcessor } from './AudioProcessor';
+
+// run scripts
+const audio = new AudioProcessor();
+audio.init();
+audio.addResultHandler('print');
