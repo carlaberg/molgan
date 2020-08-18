8add426c1cd21f38a48168d53f8b1d2c98daafce
carlaberg <mail.carl.aberg@gmail.com>
2020-08-18 23:56:28 +0200

    Added webpack with ts-loader

--- src/AudioProcessor.ts
+++ src/AudioProcessor.ts
@@ -1,12 +1,10 @@
-import { throws } from "assert";
-
 export class AudioProcessor {
   recognition: any = new (<any>window).webkitSpeechRecognition();
   result: string = '';
   resultCallbacks: any = [];
   handlerList: any;
   
-  public init = () => {
+  public init = (): void => {
     this.handlerList = { print: this.print };
 
     console.log('initiated');
@@ -15,22 +13,21 @@ export class AudioProcessor {
     console.log(this.recognition);
     this.recognition.onend = this.recognition.start;
   }
-  
-  private addEventListeners = () => {
+   
+  private addEventListeners = ():void => {
     this.recognition.onresult = this.handleResult;
     this.recognition.onerror = this.handleError;
   }
   
-  public handleResult = (e) => {
-    console.log(this)
+  public handleResult = (e: Event):void => {
     this.resultCallbacks.forEach(method => this.handlerList[method](e));
   }
   
-  public addResultHandler = (handler) => {
+  public addResultHandler = (handler: string):void => {
     this.resultCallbacks.push(handler);
   }
   
-  private print = (e) => {
+  private print = (e): void => {
     const span = document.createElement('div');
     span.innerHTML = e.results[0][0].transcript;
     document.querySelector('body').appendChild(span);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import message from './message'
-console.log(message)
-
 // import scripts
 import { AudioProcessor } from './AudioProcessor';
 
 // run scripts
 const audio = new AudioProcessor();
 audio.init();
-audio.addResultHandler('print');
+audio.addResultHandler('8');
