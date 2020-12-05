9febd3a6f51b1c4afb2fe70ba32d4fd372b117ad
carlaberg <mail.carl.aberg@gmail.com>
2020-12-05 15:16:13 +0100

    Removed logs from  example

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,5 @@ import { Molgan } from './Molgan';
 // run scripts
 const molgan = new Molgan();
 molgan.init();
-molgan.events.on('ON_RECOGNITION', (e: any) => console.log(e.result))
-console.log(molgan.events.events)
-// molgan.addResultHandler('print');
+molgan.events.on('ON_RECOGNITION', (e: any) => console.log(e.result));
+console.log(molgan.events.events);
